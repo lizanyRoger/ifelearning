03fdbbc89056ba8ad3a6a1ef52b607a77af62ccd
lizany <lizanyli@sina.com>
2017-06-29 23:32:54 +0800

    tab success,carousel a few problems

--- ife_exercise_2v2.0/main.js
+++ ife_exercise_2v2.0/main.js
@@ -1,25 +1,75 @@
-// js实现nav方法，还未完全实现
-// var droplist = document.getElementsByClassName("droplist");
-// for(var i=0;i<droplist.length;i++){
-// 	droplist[i].style.visibility = "collapse";
-// }
+// js实现nav方法
+var droplist = document.getElementsByClassName("droplist");
+for(var i=0;i<droplist.length;i++){
+	droplist[i].style.visibility = "collapse";
+}
 
-// function occur_droplist(obj){
-// 	obj.style.background = "#575757";
-// 	obj.style.height = "100%";
-// 	//obj.lastChild.style.visibility = "visible";
-// }
+function occur_droplist(obj){
+	obj.style.background = "#575757";
+	obj.style.height = "100%";
+	obj.children[1].style.visibility = "visible";
+}
 
-// function disappear_droplist(obj){
-// 	obj.style.background = "inherit";
-// 	//obj.lastChild.style.visibility = "visible";
-// }
+function disappear_droplist(obj){
+	obj.style.background = "inherit";
+	obj.children[1].style.visibility = "collapse";
+}
 
-function nav_click(obj){
+//js实现轮播图
+var wrap = document.querySelector(".carousel-wrap");
+var index = 0;
+var dots = document.getElementsByClassName("carousel-tip");
 
-	obj.nextSibling.className = "";
-	obj.className = "active";
-	document.getElementById("tab-text").innerHTML = "标签试一下"+obj;
+function showCurrentDot () {
+  for(var i = 0, len = dots.length; i < len; i++){
+    dots[i].style.className = "carousel-tip";
+  }
+  dots[index].style.className = "carousel-tip carousel-tip-active";
+  dots[index].style.background = "#4E4E4E";
+  dots[index].style.color = "#4E4E4E";
+}
+
+function next_pic () {
+      var newLeft;
+      if(wrap.style.left === "-3840px"){
+        newLeft = 0;
+      }else{
+        newLeft = parseInt(wrap.style.left)-960;
+      }
+      wrap.style.left = newLeft + "px";
+      index++;
+      if(index > 3){
+        index = 0;
+      }
+      showCurrentDot();
+    }
+
+var timer = null;
+    function autoPlay () {
+      timer = setInterval(function () {
+        next_pic();
+      },2000);
+    }
+autoPlay();
+
+//js实现tab
+var tab = document.getElementsByClassName("tab-item");
+var tab_text = document.getElementsByClassName("tab-control");
+for(var i=0;i<tab.length;i++){
+	tab[i].onclick = function(){
+		for(var j=0;j<tab.length;j++){
+			tab[j].className = "tab-item";
+			tab[j].style.background = "#fff";
+			tab[j].style.color = "#000";
+			this.className = "tab-item active";
+			this.style.background = "#3B3B3B";
+			this.style.color = "#fff";
+			tab_text[j].style.display = "none";
+			if(this == tab[j]){
+				tab_text[j].style.display = "block";
+				}
+			}		
+		}
 }
 
 function change_city(obj){
