965b9119a73062a567f1d59a8a9732ec9ecb6ab3
lizanyRoger <lizanyli@sina.com>
2017-07-08 00:38:37 +0800

    edit and delete has a few problems

--- ife_exercise_4/main.js
+++ ife_exercise_4/main.js
@@ -47,10 +47,17 @@ function recover_page_nav(){
   表格 编辑&删除
   */
 
+function hide_edit_mask(){
+	document.getElementsByClassName("mask")[0].style.display = "none";
+	document.getElementsByClassName("edit-layout")[0].style.display = "none";
+	document.getElementsByTagName("body")[0].style.overflow = "scroll";
+}
 
-function edit(){}
-
-
+function hide_delete_mask(){
+	document.getElementsByClassName("mask")[0].style.display = "none";
+	document.getElementsByClassName("delete-layout")[0].style.display = "none";
+	document.getElementsByTagName("body")[0].style.overflow = "scroll";
+}
 
 var edit_button = document.getElementsByClassName("edit");
 for(var x=0; x<edit_button.length;x++){
@@ -67,5 +74,63 @@ for(var x=0; x<edit_button.length;x++){
 		var edit_layout = document.getElementsByClassName("edit-layout")[0];
 		edit_layout.style.display = "block";
 
+		var edit_name = document.getElementsByTagName("tr")[x].children[0].innerHTML;
+		var edit_content = document.getElementsByTagName("tr")[x].children[1].innerHTML;
+		var edit_value = document.getElementsByTagName("tr")[x].children[2].innerHTML;
+		document.getElementsByName("Name")[0].value = edit_name;
+		document.getElementsByName("Content")[0].value = edit_content;
+		document.getElementsByName("Value")[0].value = edit_value;
+
+		var edit_submit = document.getElementById("submit-edit");
+		edit_submit.onclick = function edit_submit(){
+			document.getElementsByTagName("tr")[x].children[0].innerHTML = 
+				document.getElementsByName("Name")[0].value;
+			document.getElementsByTagName("tr")[x].children[1].innerHTML = 
+				document.getElementsByName("Content")[0].value;
+			document.getElementsByTagName("tr")[x].children[2].innerHTML = 
+				document.getElementsByName("Value")[0].value;
+			hide_edit_mask();
+		};
+
+	};
+
+	var edit_cancel = document.getElementById("cancel-edit");
+	edit_cancel.onclick = function edit_cancel(){
+		hide_edit_mask();
 	};
 }
+
+var delete_button = document.getElementsByClassName("delete");
+for(var x=0; x<delete_button.length;x++){
+	delete_button[x].onclick = function mask(){
+		var background = document.getElementsByClassName("mask")[0];
+		background.style.display = "block";
+		background.style.height = document.body.offsetHeight + 'px';
+		background.style.width = document.body.offsetWidth + 'px';
+		background.style.background = "rgba(125,125,125,0.5)";
+		
+		var body = document.getElementsByTagName("body")[0];
+		body.style.overflow = "hidden";
+
+		var edit_layout = document.getElementsByClassName("delete-layout")[0];
+		edit_layout.style.display = "block";
+
+		var edit_name = document.getElementsByTagName("tr")[x].children[0].innerHTML;
+		
+		document.getElementsByName("Name")[0].value = edit_name;
+		
+		var delete_submit = document.getElementById("submit-delete");
+		delete_submit.onclick = function delete_submit(){
+			//add removeNode();
+			//var delete_node = document.getElementsBy
+			//removeObj.parentNode.removeChild(removeObj)
+			hide_delete_mask();
+		};
+
+		var delete_cancel = document.getElementById("cancel-delete");
+		delete_cancel.onclick = function delete_cancel(){
+			hide_delete_mask();
+		};
+	};
+}
+
